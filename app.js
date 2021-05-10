beb689d944b5ab3a32de933d63d1e12ae6ecd87f
leoncan122 <leon.can122@gmail.com>
2021-05-11 01:13:51 +0200

    more animations

--- app.js
+++ app.js
@@ -1,21 +1,49 @@
 
 const hero = document.querySelector('.hero')
 const hamburguer = document.querySelector('#hamburguer')
-const headline = document.querySelector('.headline')
+const nav = document.querySelector('nav')
 const slider = document.querySelector('.slider')
 
-// const tl = new TimelineMax();
 
-// tl.to(hero,1, {height: "0%"}, {height: "80%", ease: power2.easeInOut})
-counter = 0 
+let counter = 0 
+
 if (counter == 0 ) {
     const id = setInterval(() => {
-        counter == 80 ? (clearInterval(id)) : (counter += 5)
+        counter == 80 ? (clearInterval(id)) : (counter += 1)
         hero.style.height = `${counter}%`
-    },30)
-    
-    
+    },17)
 }
+let counterW = 100
+setTimeout(() => {
+    if (counterW == 100) {
+        const id = setInterval(() => {
+            counterW == 80 ? (clearInterval(id)) : (counterW -= 1)
+            hero.style.width = `${counterW}%`
+        },50)
+    }
+}, 1700)
+
+let counterX = -100
+setTimeout(() => {
+    if (counterX == -100) {
+        const id = setInterval(() => {
+            counterX == 0 ? (clearInterval(id)) : (counterX += 1)
+            slider.style.left = `${counterX}%`
+        },10)
+    }
+}, 1700)
+
+let counterO = 0
+setTimeout(() => {
+    if (counterO == 0) {
+        const id = setInterval(() => {
+            counterO == 100 ? (clearInterval(id)) : (counterO += 10)
+            nav.style.opacity = `${counterO}%`
+        },100)
+    }
+}, 3000)
+
+
 
 
 
